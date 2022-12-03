aba8ef130c8b7de55484ad6f6135fd729e1c9968
yeidAguirre <yeidyaguirre@gmail.com>
2022-12-03 17:05:09 -0500

    rutas,controles,json de usuarios y editarproducto

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,7 +8,34 @@ const userController = {
 
     register: (req, res) => {
         res.render('users/register')
-    }
-}
+    },
 
+    detail:(req, res)=>{
+     let detailProfile = req.query.detail;
+     res.send(detailProfile);
+    },
+
+    create:(req, res) =>{
+    res.render(createProfile)
+    },
+
+    edit: (req, res) =>{
+         let user = req.params.id;
+         res.render = (user);
+    },
+
+    store: (req, res) =>{
+        let newUser = {
+			id: req.body.id,
+            firstname: req.body.firstname,
+            lastName: req.body.lastName,
+			email: req.body.email,
+            password: req.body.pasword,
+            category: req.body.category,
+            image: "images/avatars/pletzale",
+            stock: true,
+}
+    },
+}
 module.exports = userController
+
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -3,6 +3,16 @@ const router = express.Router();
 const userController = require('../controllers/userController')
 
 router.get('/login', userController.login);
+
 router.get('/register', userController.register);
+
+router.get('/profile/:id/', userController.detail);
+
+router.post('/profile', userController.create);
+
+router.get('/profile/:id/edit', userController.edit);
+
+router.put('/profile/:id/', userController.store);
+
 
 module.exports = router;
