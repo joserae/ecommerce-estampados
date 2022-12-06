e710b76bda40c1de2f1ab8b6fe14d83153856c24
dannavhdiaz <davahodi04@gmail.com>
2022-12-06 14:40:35 -0500

    Avances en CRUD de usuario, ajustes en vistas de registro e inicio de sesion

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const fs = require('fs');
+const users = require('../database/userData.json')
 
 const userController = {
 
@@ -10,32 +12,48 @@ const userController = {
         res.render('users/register')
     },
 
-    detail:(req, res)=>{
-     let detailProfile = req.query.detail;
-     res.send(detailProfile);
-    },
+    create: (req, res) =>{
 
-    create:(req, res) =>{
-    res.render(createProfile)
-    },
+        let newImage
+        if(req.file == undefined){
+            newImage="user.png"
+        }else{
+            newImage=req.file.filename
+        }
 
-    edit: (req, res) =>{
-         let user = req.params.id;
-         res.render = (user);
-    },
+        //save the new user
+        let usersDataFile = fs.readFileSync(path.join(__dirname,'../database/userData.json'), {encoding: 'utf-8'});
+        let oldUsersJSON;
+        let id;
+        if (usersDataFile == ""){
+            oldUsersJSON = [];
+            id = oldUsersJSON[oldUsersJSON.length - 1].id
+        } else{
+            oldUsersJSON = JSON.parse(usersDataFile);
+            id = 1;
+        }
 
-    store: (req, res) =>{
         let newUser = {
-			id: req.body.id,
-            firstname: req.body.firstname,
+            id: id + 1,
+            name: req.body.name,
             lastName: req.body.lastName,
-			email: req.body.email,
-            password: req.body.pasword,
-            category: req.body.category,
-            image: "images/avatars/pletzale",
-            stock: true,
-}
+            email: req.body.email,
+            password: req.body.password,
+            image: newImage
+        }
+
+        oldUsersJSON.push(newUser);
+        usersJSON = JSON.stringify(oldUsersJSON);
+        fs.writeFileSync(path.join(__dirname,'../database/userData.json'),usersJSON);
+
+        res.redirect('/users/register')
+
     },
+
+    list: (req,res) => {
+        res.render('users/userList', {users})
+    }
+
 }
 module.exports = userController
 
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
 const userController = require('../controllers/userController')
+
+const storage = multer.diskStorage({
+	destination: (req, file, callback) =>{
+		let folder = path.join(__dirname, "../../public/img/usersAvatars");
+
+		callback(null, folder)
+	},
+    filename: (req, file, callback) =>{
+        let imageName = Date.now() + path.extname(file.originalname);
+
+		callback(null, imageName);
+    }
+});
+
+const fileUpload = multer({ storage: storage });
 
 router.get('/login', userController.login);
 
+//formulario de registro
 router.get('/register', userController.register);
-
-router.get('/profile/:id/', userController.detail);
-
-router.post('/profile', userController.create);
-
-router.get('/profile/:id/edit', userController.edit);
-
-router.put('/profile/:id/', userController.store);
-
+//guardar el nuevo usuario
+router.post('/register', fileUpload.single('userAvatar'), userController.create);
+//interfaz del CRUD de usuarios
+router.get('/userList', userController.list);
 
 module.exports = router;
