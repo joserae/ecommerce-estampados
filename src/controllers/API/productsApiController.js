fdc161270d2a2c86f9098f522ab9e85f1fe9927b
dannavhdiaz <davahodi04@gmail.com>
2023-02-23 19:45:54 -0500

    Ajustes a los API Sprint 8 - se agrega paginado

--- src/controllers/API/productsApiController.js
+++ src/controllers/API/productsApiController.js
@@ -13,63 +13,174 @@ const dbGenres = db.Genre;
 const dbProducts = db.Product;
 const dbSizes = db.Size;
 const dbStock = db.Stock;
+const limit = 10
 
 const producstApiController = {
 
     list: async (req, res) =>{
         
-        let allProducts = await dbProducts.findAll({
-            attributes: ['id','name','description']
-        });
+        try {
+            let page = req.query.page
 
-        let products =  allProducts.map( element => {
-            return {...element['dataValues'],
-                detail : urlApi + '/' + element['dataValues']['id']}
-        })
+            //si no se envía la página, por defecto es la 1
+            if(!page){
+                page = 1
+            }
 
-        let countByCategoryAllProducts = await dbProducts.findAll({
-            attributes: [
-                [sequelize.fn('COUNT', sequelize.col('Category.id')),'total']
-            ],
-            include: [
-                {model: dbCategories,
-                    required: true,
-                    attributes: ['name']
-                }               
-            ],
-            group: `Category.id`
-        })
+            let countProducts = await dbProducts.count()
+            let cantPages = countProducts / limit
+            cantPages = Number.isInteger(cantPages) ? cantPages : Math.floor(cantPages + 1) 
 
-        let countByCategory = Object.fromEntries(
-            Object.values(countByCategoryAllProducts).map(element => {
-                return  [element['dataValues']['Category']['name'],element['dataValues']['total']]
-            })
-        )
+            //si la petición se realiza dentro de los rangos
+            if (page > 0 && page <= cantPages){
 
-        res.json({
-                count: products.length,
-                countByCategory: countByCategory,
-                products: products
-            })
+                let offset = page * limit - limit
 
+                let allProducts = await dbProducts.findAll({
+                    attributes: ['id','name','description'],
+                    limit: limit,
+                    offset: offset
+                });
+
+                //añadir el detail a cada producto
+                let products =  allProducts.map( element => {
+                    return {
+                        ...element['dataValues'],
+                        detail : urlApi + '/' + element['dataValues']['id']
+                    }
+                })
+
+                let countByCategoryAllProducts = await dbProducts.findAll({
+                    attributes: [
+                        [sequelize.fn('COUNT', sequelize.col('Category.id')),'total']
+                    ],
+                    include: [
+                        {model: dbCategories,
+                            required: true,
+                            attributes: ['name']
+                        }               
+                    ],
+                    group: 'Category.id'
+                })
+
+                let countByCategory = Object.fromEntries(
+                    Object.values(countByCategoryAllProducts).map(element => {
+                        return  [element['dataValues']['Category']['name'],element['dataValues']['total']]
+                    })
+                )
+
+                let nextPage = parseInt(page) + 1
+                let previousPage = parseInt(page) - 1
+                let urlNextPage = urlApi + `/?page=${nextPage}`
+                let urlPreviousPage = urlApi + `/?page=${previousPage}`
+
+                let data = {
+                    count: countProducts,
+                    countByCategory: countByCategory,
+                    next : urlNextPage,
+                    previous : urlPreviousPage,
+                    products: products
+                }
+
+                //Primera y última página - No requiere previous ni next url
+                if (page == 1 && page == cantPages){
+                    data.next = null
+                    data.previous = null
+                }
+                //Primera página - No requiere previous url
+                else if (page == 1){
+                    data.previous = null
+                }
+                //Última página - No requiere next url
+                else if (page == cantPages){
+                    data.next = null
+                }
+
+                res.json(data)
+            }
+            //Responder con error
+            else{
+                res.status(404).json({
+                    message: "404 Not Found",
+                    status: 404})
+            }
+        }
+        catch (e) {
+            console.log({ name: e.name, message: e.message})
+            res.status(404).json({
+                message: "404 Not Found",
+                status: 404})
+        }
     },
 
     detail: async (req, res) =>{
 
-        let oneProduct = await dbProducts.findByPk(req.params.id,{
-            attributes: ['id','name','description','price','img','brand_id','category_id','genre_id']
-        })
+        try {
 
-        let productSizes = await dbStock.findAll({
-            where: {product_id: req.params.id}
-        })
+            let oneProduct = await dbProducts.findByPk(req.params.id,{
+                attributes: ['id','name','description','price','img','created_date'],
+                include: [
+                    {model: dbBrands,
+                        required: true,
+                        attributes: ['name']
+                    },
+                    {model: dbCategories,
+                        required: true,
+                        attributes: ['name']
+                    },
+                    {model: dbGenres,
+                        required: true,
+                        attributes: ['name']
+                    }           
+                ]
+            })
+            
+            if (oneProduct) {
 
-        let product = {...oneProduct['dataValues'],img : urlImg + '/' + oneProduct['dataValues']['img']}
+                let productSizes = await dbStock.findAll({
+                    attributes: ['available_quantity'],
+                    include: [
+                        {model: dbSizes,
+                            required: true,
+                            attributes: ['size']
+                        }
+                    ],
+                    where: {product_id: req.params.id}
+                })
         
-        res.json({
-            product: product,
-            sizes: productSizes
-        })
+            //Ordenar el objeto de Stock
+               let Stock = Object.values(productSizes).map(element => {
+                    let available_quantity = element['dataValues']['available_quantity']
+                    let size = element['dataValues']['Size']['dataValues']['size']
+                    return {
+                        size,
+                        available_quantity
+                    }
+                })
+            
+                let product = {...oneProduct['dataValues'],
+                    img : urlImg + '/' + oneProduct['dataValues']['img'],
+                    Stock : Stock,
+                    Brand: oneProduct['Brand']['dataValues']['name'],
+                    Category: oneProduct['Category']['dataValues']['name'],
+                    Genre: oneProduct['Genre']['dataValues']['name']
+                }
+                
+                res.json(product)
+
+            }else{
+                res.status(404).json({
+                    message: "404 Not Found",
+                    status: 404})
+            }
+            
+        }
+        catch (e) {
+            console.log({ name: e.name, message: e.message})
+            res.status(404).json({
+                message: "404 Not Found",
+                status: 404})
+        }
 
     }
     
--- src/controllers/API/usersApiController.js
+++ src/controllers/API/usersApiController.js
@@ -9,37 +9,118 @@ const db = require('../../database/models/index.js');
 const { count } = require('console');
 const dbUsers = db.User
 const dbRole = db.Role
+const limit = 10
 
 const usersApiController = {
 
     list: async (req, res) =>{
         
-        let allUsers = await dbUsers.findAll({
-            attributes: ['id','first_name','last_name','email']
-        });
+        try{
 
-        let users = allUsers.map( element => {
-            return {...element['dataValues'], detail : urlApi + '/' + element['dataValues']['id']}
-        })
+            let page = req.query.page
 
-        res.json({
-                count: users.length,
-                users: users
-            })
+            //si no se envía la página, por defecto es la 1
+            if(!page){
+                page = 1
+            }
+
+            let countUsers = await dbUsers.count()
+            let cantPages = countUsers / limit
+            cantPages = Number.isInteger(cantPages) ? cantPages : Math.floor(cantPages + 1) 
+
+            //si la petición se realiza dentro de los rangos
+            if (page > 0 && page <= cantPages){
+            
+                let offset = page * limit - limit
+
+                let allUsers = await dbUsers.findAll({
+                    attributes: ['id','first_name','last_name','email'],
+                    limit: limit,
+                    offset: offset
+                });
+
+                //añadir el detail a cada usuario
+                let users = allUsers.map( element => {
+
+                    let detail = urlApi + '/' + element['dataValues']['id']
+
+                    return {
+                        ...element['dataValues'],
+                        detail : detail
+                    }
+                })
+
+                let nextPage = parseInt(page) + 1
+                let previousPage = parseInt(page) - 1
+                let urlNextPage = urlApi + `/?page=${nextPage}`
+                let urlPreviousPage = urlApi + `/?page=${previousPage}`
+
+                let data = {
+                    count: countUsers,
+                    next : urlNextPage,
+                    previous : urlPreviousPage,
+                    users: users
+                }
+
+                //Primera y última página - No requiere previous ni next url
+                if (page == 1 && page == cantPages){
+                    data.next = null
+                    data.previous = null
+                }
+                //Primera página - No requiere previous url
+                else if (page == 1){
+                    data.previous = null
+                }
+                //Última página - No requiere next url
+                else if (page == cantPages){
+                    data.next = null
+                }
+                
+                res.json(data)
+
+            } 
+            //Responder con error
+            else{
+                res.status(404).json({
+                    message: "404 Not Found",
+                    status: 404})
+            }
+        }
+        catch (e) {
+            console.log({ name: e.name, message: e.message})
+            res.status(404).json({
+                message: "404 Not Found",
+                status: 404})
+        }
 
     },
 
     detail: async (req, res) =>{
 
-        let oneUser = await dbUsers.findByPk(req.params.id,{
-            attributes: ['id','first_name','last_name','email','avatar_img']
-        })
+        try {
+            let oneUser = await dbUsers.findByPk(req.params.id,{
+                attributes: ['id','first_name','last_name','email','avatar_img']
+            })
 
-        let user = {...oneUser['dataValues'],avatar_img : urlAvatars + '/' + oneUser['dataValues']['avatar_img']}
-        
-        res.json({
-            user: user
-        })
+            if (oneUser){
+                let user = {...oneUser['dataValues'],avatar_img : urlAvatars + '/' + oneUser['dataValues']['avatar_img']}
+            
+                res.json(user)
+
+            }else{
+                res.status(404).json({
+                    message: "404 Not Found",
+                    status: 404})
+            }
+
+        }
+        catch (e) {
+            console.log({ name: e.name, message: e.message})
+            res.status(404).json({
+                message: "404 Not Found",
+                status: 404})
+        }
+
 
     }
     
