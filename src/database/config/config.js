13be485c5f118a7774939d1fc3862a484c1392ad
joserae <josepablorestrepo@gmail.com>
2023-09-20 21:28:31 -0500

    rolling back to previous original state

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,39 +1,27 @@
-const PORT = process.env.PORT || 3000;
-
-let MYSQLUSER = "root";
-let MYSQLPORT = "7662";
-let MYSQLPASSWORD = "GkIrQXRmUacMkEPdBZI3";
-let MYSQLHOST = "containers-us-west-108.railway.app"
-let MYSQLDATABASE = "railway";
-let MYSQLURL = "mysql://root:GkIrQXRmUacMkEPdBZI3@containers-us-west-108.railway.app:7662/railway";
-
 const CONFIG = {
   "development": {
-    "username": MYSQLUSER,
-    "password": MYSQLPASSWORD || null,
-    "database": MYSQLDATABASE || "one_step",
-    "host": MYSQLHOST || "127.0.0.1",
+    "username": "root",
+    "password": null,
+    "database": "one_step",
+    "host": "127.0.0.1",
     "dialect": "mysql",
-    "port": MYSQLPORT || "3307",
-    "url": MYSQLURL
+    "port": "3307"
   },
   "test": {
-    "username": MYSQLUSER,
-    "password": MYSQLPASSWORD || null,
-    "database": MYSQLDATABASE || "one_step",
-    "host": MYSQLHOST || "127.0.0.1",
+    "username": "root",
+    "password": null,
+    "database": "one_step",
+    "host": "127.0.0.1",
     "dialect": "mysql",
-    "url": MYSQLURL
   },
   "production": {
-    "username": MYSQLUSER,
-    "port": MYSQLPORT,
-    "password": MYSQLPASSWORD,
-    "database": MYSQLDATABASE,
-    "host": MYSQLHOST,
+    "username": "root",
+    "port": "3307",
+    "password": null,
+    "database": "one_step",
+    "host": "127.0.0.1",
     "dialect": "mysql",
-    "url": "mysql://root:GkIrQXRmUacMkEPdBZI3@containers-us-west-108.railway.app:7662/railway"
   }
 }
 
-module.exports = CONFIG, PORT;
+module.exports = CONFIG;
